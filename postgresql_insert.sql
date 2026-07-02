907cc053ac7a1aaa08597ac23fce040411932cac
Manish Kumar <manishkumarg7394@gmail.com>
2026-07-02 16:01:15 +0530

    Create postgresql_insert.sql

---
+++ postgresql_insert.sql
@@ -0,0 +1,14 @@
+create table blinkit(
+Item_Fat_Content varchar (10),
+Item_Identifier varchar (10),
+Item_Type varchar (50),
+Outlet_Establishment_Year int,
+Outlet_Identifier varchar (10),
+Outlet_Location_Type varchar (10),
+Outlet_Size varchar (10),
+Outlet_Type varchar (50),
+Item_Visibility float,
+Item_Weight  float,
+Sales float,
+Rating float
+)
